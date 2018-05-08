6aa0004ebe7e9f112e5c31dde937b530f7b91c95
Shlomi Fish <shlomif@shlomifish.org>
2018-05-08 20:27:03 +0300

    Add the first version.

---
+++ Code-TidyAll-Plugin-Flake8/lib/Code/TidyAll/Plugin/Flake8.pm
@@ -0,0 +1,35 @@
+package Code::TidyAll::Plugin::Flake8;
+
+use Moo;
+use String::ShellQuote qw/ shell_quote /;
+
+extends 'Code::TidyAll::Plugin';
+
+sub validate_file
+{
+    my ( $self, $fn ) = @_;
+    my $cmd = shell_quote( 'flake8', $fn );
+    if (`$cmd`)
+    {
+        die 'not valid';
+    }
+    return;
+}
+
+1;
+
+=head1 NAME
+
+Code::TidyAll::Plugin::Flake8 - run flake8 using Code::TidyAll
+
+=head1 SYNOPSIS
+
+In your C<.tidyallrc>:
+
+    [Flake8]
+    select = **/*.py
+
+=head1 DESCRIPTION
+
+This speeds up the flake8 python tool ( L<http://flake8.pycqa.org/en/latest/>
+) checking by caching results using L<Code::TidyAll> .
